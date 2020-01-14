ae24493c546548feced9ed40d68fa39e36e0626a
DaryaYa <lyalyaka@mail.ru>
2020-01-14 12:45:41 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-
-
-exports.cToF = function (c) {
+exports.cToF = function(c) {
+    // (0 °C × 9/5) + 32 = 32 °F
+    const f = (c * 9 / 5) + 32;
+    return f.toFixed(2);
 
 }
 
-exports.fToC = function (f) {
-
+exports.fToC = function(f) {
+    // (32 °F − 32) × 5/9 = 0 °C
+    const c = (f - 32) * 5 / 9;
+    return c.toFixed(2);
 }
